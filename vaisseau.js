f004be6d031a21ab4111ba8e7a619d6c2290d9eb
gasparclbx <gaspar.clbx@gmail.com>
2021-12-15 00:10:20 +0100

    Changement lisibilité & ajout déplacement vaisseau

---
+++ vaisseau.js
@@ -0,0 +1,50 @@
+// Création d'une fonction créant nos entités
+function Sprite(filename, left, top, height) {
+    this.ship = document.createElement("img");
+    this.ship.src = filename;
+    this.ship.style.position = "absolute";
+    document.body.appendChild(this.ship);
+// Ajout d'un argument pour la position sur la verticale
+    Object.defineProperty(this, "left", {
+        get: function() {
+            return this._left;
+        },
+        set: function(value) {
+            this._left = value;
+            this.ship.style.left = value + "px";
+        }
+    });
+// Ajout d'un argument pour la position sur l'horizontale
+    Object.defineProperty(this, "top", {
+        get: function() {
+            return this._top;
+        },
+        set: function(value) {
+            this._top = value;
+            this.ship.style.top = value + "px";
+        }
+    });
+// Ajout d'un argument pour l'apparition
+    Object.defineProperty(this, "display", {
+        get: function() {
+            return this.ship.style.display;
+        },
+        set: function(value) {
+            this.ship.style.display = value;
+        }
+    });
+// Ajout d'un argument pour la 
+    Object.defineProperty(this, "height", {
+        get: function() {
+            return this._height;
+        },
+        set: function(value) {
+            this._height = value;
+            this.ship.style.height = value + "px";
+        }
+
+    });
+    this.height = height 
+    this.left = left;
+    this.top = top;
+}
